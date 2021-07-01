26f88d55cf46eb1433c273a217e8578bb41ea5f0
imrkundu599 <63168471+imrkundu599@users.noreply.github.com>
2021-07-01 12:05:18 +0530

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -7,27 +7,13 @@ import FD from './components/FD.vue';
 import RD from './components/RD.vue';
 import dropdown from './components/dropdown.vue';
 
-/*import Services from './components/Services.vue';
-import About from './components/About.vue';
 
-import Request from './components/Request.vue';
-import Pricing from './components/Pricing.vue';
-import Testimonial from './components/Testimonial.vue';
-import Contact from './components/Contact.vue';*/
-
-//import Video from './components/Video.vue';
 
 
 export default[
     {path: '/',component: Home},
-   // {path: '/services',component: Services},
-  //  {path: '/about',component: About},
     {path: '/dropdown',component: dropdown},
-  //  {path: '/request',component: Request},
-  //  {path: '/pricing',component: Pricing},
-  //  {path: '/testimonial',component: Testimonial},
-  //  {path: '/contact',component: Contact},
-  //  {path: '/video',component: Video},
+
     {path: '/register',component: Register},
     {path: '/login',component: Login},
     {path: '/userhome',component: UserHome},
